37169121edb6e5f94683385742dc6ffd1b0670a3
EQUASHNZRKUL <justinkae@gmail.com>
2019-06-06 14:24:07 -0400

    copied placeonplane

--- ARPushable.cs
+++ ARPushable.cs
@@ -1,67 +1,41 @@
-using System;
 using System.Collections.Generic;
-// using Unity.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 /// <summary>
-/// Represents a ball moved by raycasts/taps that is created when raycast intersects a plane. 
+/// Listens for touch events and performs an AR raycast from the screen touch point.
+/// AR raycasts will only hit detected trackables like feature points and planes.
+///
+/// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
+/// and moved to the hit position.
 /// </summary>
 [RequireComponent(typeof(ARRaycastManager))]
-public class ARPushable : MonoBehaviour
+public class PlaceOnPlane : MonoBehaviour
 {
-    // Obj prefab ref
     [SerializeField]
-    [Tooltip("Instantiates this prefab on a plane at touch location")]
-    GameObject arPrefab;
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
 
-    /// The Prefab to instantiate on touch.
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
     public GameObject placedPrefab
     {
-        get { return arPrefab; }
-        set { arPrefab = value; }
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
     }
 
-    // Visible, spawned object ref
-    public GameObject spawnedObject {get; private set; }
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
 
-    // Event to invoke for placing objects on planes.
-    // public static event Action planePlacedObject;
-
-    // Session Origin ref; used to make raycasts
-    private ARSessionOrigin sessionOrigin;
-
-    // Ray Cast manager ref
-    ARRaycastManager RaycastManager; 
-
-    // Frag if object was placed or it should be moved
-    // private bool objectPlaced = false;
-
-    // Bool if moving or not (needed for Update())
-    private bool objectMotion = false;
-
-    // Thrust value attached to the object
-    // public float thrust;
-
-    void Start()
-    {
-    // Ref to Ar session origin within GameObject
-    Debug.Log("Start Test");
-    sessionOrigin = GetComponent<ARSessionOrigin>();
-
-    // Instance of object to be hidden until placed. 
-    // spawnedObject = Instantiate(arPrefab);
-    // spawnedObject.gameObject.SetActive(false);
-    }
-
-    // Awake is run on Load.
     void Awake()
     {
-        RaycastManager = GetComponent<ARRaycastManager>(); 
+        m_RaycastManager = GetComponent<ARRaycastManager>();
     }
 
-    //TODO: What is out?
     bool TryGetTouchPosition(out Vector2 touchPosition)
     {
 #if UNITY_EDITOR
@@ -84,40 +58,28 @@ public class ARPushable : MonoBehaviour
     }
 
     void Update()
-    {   
-        // Check if object is moving, if so then move it
-        // if (objectMotion)
-        //     spawnedObject.gameObject.transform += Vector3.up * 10.0f;
-
-        // Checks if screen pressed
+    {
         if (!TryGetTouchPosition(out Vector2 touchPosition))
             return;
 
-        // List of AR Hits
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
 
-        if (RaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
-        { // If raycast hits a plane
-            Debug.Log("HIT");
-            // Find first hit
-            // hits.OrderBy(h => h.distance);
-            var pose = hits[0].pose;
-            var hittype = hits[0].hitType;
-            // TODO: this is gonna need to change once multiples added
-            if (spawnedObject == null) 
-                Debug.Log("Instantiate");
-                spawnedObject = Instantiate(arPrefab, pose.position, pose.rotation);
-        }
-        else 
-        // if (RaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinInfinity))
-        { // if raycast hits anything else
-            var pose = hits[0].pose;
-            var hittype = hits[0].hitType;
-            Debug.Log("MISS");
-            // TODO: This is where the physics happens
-            Vector3 cam_pos = sessionOrigin.camera.transform.position;
-            Debug.DrawLine(cam_pos, pose.position, Color.red);
-            objectMotion = true;
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
         }
     }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    ARRaycastManager m_RaycastManager;
 }
