d6edee7548e8ed4ff6b6c48463b6dab21636bfd2
EQUASHNZRKUL <justinkae@gmail.com>
2019-06-12 12:16:01 -0400

    First attempt at XRCameraImage

--- CameraImage_test.cs
+++ CameraImage_test.cs
@@ -12,6 +12,7 @@ using UnityEngine.XR.ARSubsystems;
 /// and moved to the hit position.
 /// </summary>
 [RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARCameraManager))]
 public class CameraImage_test : MonoBehaviour
 {
     [SerializeField]
@@ -51,19 +52,41 @@ public class CameraImage_test : MonoBehaviour
 
     public float JUMP_FORCE;
 
+    public 
+
     void Awake()
     {
         // Console.WriteLine("StartTest");
         Debug.Log("StartTest");
         m_ARRaycastManager = GetComponent<ARRaycastManager>();
         m_SessionOrigin = GetComponent<ARSessionOrigin>();
+        m_ARCameraManager = GetComponent<ARCameraManager>();
     }
 
     void Update()
     {
+        // CAMERA IMAGE HANDLING
+        XRCameraImage image;
+        if (m_ARCameraManager.TryGetLatestImage(out image))
+        {
+            Debug.LogFormat("Dimensions: {0}\n\t Format: {1}\n\t Time: {2}\n\t ", 
+                image.dimensions, image.format, image.timestamp);
+            for (int planeIndex = 0; planeIndex < image.planeCount; ++planeIndex)
+            {
+                // Log information about image plane
+                XRCameraImagePlane plane = image.GetPlane(planeIndex);
+                Debug.LogFormat("Plane {0}:\n\tsize: {1}\n\tpixelStride{2}", 
+                    planeIndex, plane.data.Length, plane.pixelStride);
+                
+                // Do something with the data: 
+                // MyCVAlgo(plane.data);
+            }
+
+            image.Dispose();
+        }
+
+        // TOUCH HANDLING
         // Checks for inputs
-        // if (!TryGetTouchPosition(out Vector2 touchPosition))
-        //     return;
         if (Input.touchCount <= 0)
             return;
         Touch touch = Input.GetTouch(0);
@@ -72,7 +95,6 @@ public class CameraImage_test : MonoBehaviour
             // Distance calculations
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
-            // Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue);
             bool physRayBool = Physics.Raycast(ray, out hit);
             bool arRayBool = m_ARRaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon);
             if (physRayBool) 
@@ -80,13 +102,9 @@ public class CameraImage_test : MonoBehaviour
                 Collider spawnedCollider = hit.collider;
                 if ((arRayBool && spawnedCollider.gameObject.tag == "AR Placed Object") || (!arRayBool))
                 { // Hits a spawned object
-                    // var hitPose = hit.transform;
-                    // testObject = Instantiate(m_PhysicalPrefab, hit.point, hitPose.rotation);
                     Debug.Log(hit.rigidbody);
                     Vector3 forceDirection = hit.normal * -1.0f;
                     spawnedObject.GetComponent<Rigidbody>().AddForce(forceDirection*JUMP_FORCE);
-                    // hit.rigidbody.AddForce(forceDirection*JUMP_FORCE);
-                    // spawnedObject.transform.Translate(forceDirection * 0.1f);
                 }
                 else if (spawnedCollider.gameObject.tag == "Plane Spawn") 
                 { // Hits the plane
@@ -105,11 +123,12 @@ public class CameraImage_test : MonoBehaviour
                 }
             }
         }
-
     }
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 
     ARRaycastManager m_ARRaycastManager;
+
+    ARCameraManager m_ARCameraManager;
 
     ARSessionOrigin m_SessionOrigin;
 }
